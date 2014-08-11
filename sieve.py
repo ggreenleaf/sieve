a58aa000d400f2ba5bc0d6e74120a7b034d7c896
Geoffrey <geoffrey.greenleaf@gmail.com>
2014-08-11 14:30:16 -0500

    changed range to xrange and fixed end of 1st for

--- sieve.py
+++ sieve.py
@@ -1,18 +1,25 @@
 def sieve (n):
-	num_dict = dict([num,True] for num in range(1,n+1))
+	num_dict = dict([num,True] for num in xrange(1,n+1))
 
-	for i in range(2, int(n**.5) ):
+	for i in xrange(2, int(n**.5) + 1 ):
 		if num_dict [i]:
-			for j in range(i*2,n+1,i):
+			for j in xrange(i*2,n+1,i):
 				num_dict[j] = False
 	
 
 
-	return [i for i in range(1,n+1) if num_dict[i]]
+	return [i for i in xrange(1,n+1) if num_dict[i]]
 
 while True:
 	num = raw_input ("Enter a number or end to quit: ")
-	if num.lower() == "end":
-		break;
-	else:
+	
+	try:
+		int(num)
 		print sieve(int(num))
+	
+	except ValueError:
+		if num.lower() == "end":
+			break
+		else:
+			print ("Not a valid number please re-enter")
+	
